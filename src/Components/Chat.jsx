e564e09fd4c9b9d5b46541f68017ecaab4dab4a5
pratikpatil552 <112548965+pratikpatil552@users.noreply.github.com>
2024-01-24 17:15:05 +0530

    Update Chat.jsx

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -21,7 +21,7 @@ const Chat = () => {
     },[])
 
     function connectTows (){
-      const ws = new WebSocket('https://mcchatapi.azurewebsites.net');
+      const ws = new WebSocket('wss://mcchatapi.azurewebsites.net');
       setWs(ws);
       ws.addEventListener('message',handleMessage);
       ws.addEventListener('close',()=>{
