ae9aaadd37e6c1a7d256c889f4dcf5149fd60772
Pratik Patil <pratikpatilmudalp3@gmail.com>
2024-01-24 16:43:27 +0530

    frontend added

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -21,7 +21,6 @@ const Chat = () => {
     },[])
 
     function connectTows (){
-      // const ws = new WebSocket('ws://https://mcchatapi.azurewebsites.net');
       const ws = new WebSocket('ws://localhost:8000');
       setWs(ws);
       ws.addEventListener('message',handleMessage);
@@ -68,15 +67,18 @@ const Chat = () => {
       setId(null);
       setUsername(null);
       console.log(response.data);
+      setWs(null);
     }
 
-    function sendMessage(ev){
-      ev.preventDefault();
+
+    function sendMessage(ev,file=null){
+      if(ev) ev.preventDefault();
       console.log("sending message");
       ws.send(JSON.stringify(
         {
           recipient : selectedUserId,
           text : newMessageText,
+          file,
         }
       ));
       setMessages(prev => [...prev,{
@@ -177,7 +179,14 @@ const Chat = () => {
           ))
         }
         </div>
-        <div className='p-2 text-center'>
+        <div className='p-2 text-center flex items-center justify-center'>
+          <span className='mr-2 text-sm text-gray-600 flex items-center'>
+          <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
+            {username}
+          </span>
           <button
             onClick={logout}
            className='text-sm text-gray-500 bg-blue-100 py-1 px-2 rounded-md'>Logout</button>
@@ -217,6 +226,7 @@ const Chat = () => {
                   <input type="text" placeholder='Type your message here' 
                   value={newMessageText} onChange={ev => setNewMessageText(ev.target.value)}
                   className='bg-white border p-2 flex-grow rounded-lg' />
+            
                   <button type='submit' className='bg-blue-500 p-2 text-white rounded-lg '>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
